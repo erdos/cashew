81503be9ab87056cafcb29ae32b102a797e925c0
Janos Erdos <janos@transferwise.com>
2021-03-25 00:21:54 +0100

    set warn-on-reflection in project.clj

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
+  :global-vars {*warn-on-reflection* true}
   :repl-options {:init-ns cashew.core})
